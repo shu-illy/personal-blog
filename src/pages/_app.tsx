d4b164b0a23d0c251eb17962bb4382de0526ad85
shu-illy <sh.illy.9117@gmail.com>
2022-07-03 12:17:32 +0900

    fontFamilyを設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,10 @@ function MyApp({ Component, pageProps }: AppProps) {
       emotionOptions={{ key: "mantine", prepend: false }}
       theme={{
         primaryColor: "teal",
+        // 参考:
+        // https://www.rivfield.com/famous-sites-css-font-family/
+        fontFamily:
+          "Helvetica Neue ,Arial, Hiragino Kaku Gothic ProN, Hiragino Sans, Meiryo,sans-serif",
       }}
     >
       <Component {...pageProps} />
