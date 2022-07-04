b15676b0d2cb7331f48323abcc5a18f0474d4c86
shu-illy <sh.illy.9117@gmail.com>
2022-07-05 01:45:47 +0900

    記事詳細ページのルーティング追加

---
+++ src/pages/articles/[id].tsx
@@ -0,0 +1,21 @@
+import { Container, Grid } from "@mantine/core";
+import type { NextPage } from "next";
+import Image from "next/image";
+import IndexBody from "src/components/organisms/index/IndexBody";
+import IndexSideBar from "src/components/organisms/index/IndexSideBar";
+import Layout from "src/components/templates/Layout";
+import { Article } from "src/types";
+
+type Props = {
+  article: Article;
+};
+
+const ArticleShow: NextPage<Props> = ({ article }: Props) => {
+  return (
+    <Layout title={"記事詳細ページ"}>
+      <div className="h-5 bg-teal-200"></div>
+    </Layout>
+  );
+};
+
+export default ArticleShow;
