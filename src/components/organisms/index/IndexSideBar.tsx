91086fd525a39bf541f516a800edc00464ea9b35
shu-illy <sh.illy.9117@gmail.com>
2022-07-05 01:27:22 +0900

    TODOコメント追加

--- src/components/organisms/index/IndexSideBar.tsx
+++ src/components/organisms/index/IndexSideBar.tsx
@@ -6,10 +6,8 @@ const IndexSideBar = () => {
   return (
     <>
       <ProfileCard profile={PROFILE} />
-      <div className="m-2 bg-red-200 text-4xl  font-extrabold">
-        カテゴリ一覧
-      </div>
-      <div className="m-2 bg-green-200 text-4xl font-extrabold">アーカイブ</div>
+      {/* TODO カテゴリ一覧 */}
+      {/* TODO アーカイブ */}
     </>
   );
 };
