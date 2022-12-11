c915f3d755249db9a012c2a2cb1845db1710e5bd
shu-illy <sh.illy.9117@gmail.com>
2022-12-12 07:01:46 +0900

    analytics設定
    参考:
    https://zenn.dev/taichifukumoto/scraps/78e2f13e923b06

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import "src/lib/tailwind.css";
 import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
+import Script from "next/script";
+import { usePageView } from "src/hooks/usePageView";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  usePageView();
   return (
     <MantineProvider
       withGlobalStyles
@@ -11,8 +14,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         primaryColor: "teal",
         // 参考:
         // https://www.rivfield.com/famous-sites-css-font-family/
-        fontFamily:
-          "Helvetica Neue ,Arial, Hiragino Kaku Gothic ProN, Hiragino Sans, Meiryo,sans-serif",
+        fontFamily: "Helvetica Neue ,Arial, Hiragino Kaku Gothic ProN, Hiragino Sans, Meiryo,sans-serif",
       }}
     >
       <Component {...pageProps} />
